4a2dee88a08dd3b8de46dd8c12972cd978e18301
nikhilc52 <nsc49@cornell.edu>
2024-10-15 23:48:52 -0400

    updated website

---
+++ website/tooltips.js
@@ -0,0 +1,12 @@
+function myFunction(e, tooltipID, elementID) {
+    var x = e.clientX; //x is the distance from the left of the screen
+    var y = e.clientY; //y is the distance from the top of the screen
+    
+    //places the tool tip at the pixel location along the canvas
+    //since window.scrollX says how far along we are on the page. offset by the position of the element we're hovering over (offsetLeft)
+    document.getElementById(tooltipID).style.left = x+window.scrollX-document.getElementById(elementID).offsetLeft+20 + "px"; 
+    
+    //places the tool tip at the pixel location along the canvas (relative to the top of the page)
+    // offset by the position of the element we're hovering over (offsetTop)
+    document.getElementById(tooltipID).style.top = y-document.getElementById(elementID).offsetTop+20 + "px";
+}
